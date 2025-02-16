405788a903547d5d3abbf6ff9ee9a20aed4c2c1a
risetechapps <apps@risetech.com.br>
2025-02-16 18:12:28 -0300

    Atualizado rotas

--- src/routes/routes.php
+++ src/routes/routes.php
@@ -2,17 +2,9 @@
 
 
 use Illuminate\Support\Facades\Route;
-use RiseTechApps\ApiKey\Http\Controllers\Authentication\AuthController;
-use RiseTechApps\ApiKey\Http\Controllers\Authentication\ProfileController;
-use RiseTechApps\ApiKey\Http\Controllers\Dashboard\Modules\ModulesController;
-use RiseTechApps\ApiKey\Http\Controllers\Dashboard\Plans\PlansController;
 
 
-Route::middleware(['api'])->prefix('api/v1/')->group(function () {
-
+Route::middleware(['api', 'plan'])->prefix('api/v1/')->group(function () {
 
     RiseTechApps\ApiKey\AuthApiKey::routes();
-
-
-    Route::get('test', [\App\Http\Controllers\ClientController::class, 'test']);
 });
