5b11561d4d31e88b0fd1b5ec4bce7ad6cdec1729
risetechapps <apps@risetech.com.br>
2025-02-20 15:49:30 -0300

    Corrigido validação de requisição

--- src/Http/Middlewares/CheckRequestLimitMiddleware.php
+++ src/Http/Middlewares/CheckRequestLimitMiddleware.php
@@ -16,36 +16,12 @@ class CheckRequestLimitMiddleware
         $requestsMade = $user->countUsed();
         $requestsLimit = $user->requestLimit();
 
-        if ($requestsMade >= $requestsLimit) {
+        if ($requestsMade >= $requestsLimit && $requestsLimit > 0) {
             return response()->json(['error' => 'Limite de requisições atingido'], 429);
         }
 
         $user->requestUsed();
 
         return $next($request);
-
-
-//        $user = $request->user();
-//
-//        if (!$user || !$user->plan || !$user->plan->isActive()) {
-//            return response()->json(['error' => 'Usuário sem plano ativo'], 403);
-//        }
-//
-//        $userPlan = $user->userPlan;
-//
-//        if ($userPlan->requests_used >= $userPlan->plan->request_limit) {
-//            return response()->json(['error' => 'Limite de requisições atingido'], 429);
-//        }
-//
-//        $response = $next($request);
-//
-//        RequestLog::create([
-//            'user_id' => $user->id,
-//            'endpoint' => $request->path(),
-//        ]);
-//
-//        $userPlan->increment('requests_used');
-//
-//        return $response;
     }
 }
