d4c9e45f0b339907d272cafc6da595fd9dfec214
risetechapps <apps@risetech.com.br>
2025-11-27 17:07:51 -0300

    Criado repository de Module e Plan

---
+++ src/Repositories/Module/ModuleRepository.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace RiseTechApps\ApiKey\Repositories\Module;
+
+use RiseTechApps\Repository\Contracts\RepositoryInterface;
+
+interface ModuleRepository extends RepositoryInterface
+{
+
+}
